f5120b7bc4c653e38c888297948ac74dcc341c5d
kushal-sinha <kushal147258369@gmail.com>
2024-10-07 19:11:52 +0530

    line 11 creates a circle

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import { motion } from 'framer-motion'
+import { styles } from '../style'
+import { ComputersCanvas } from './canvas'
+//this basica;;y creates a circle line no-11
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='relative w-full h-screen mx-auto'>
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max- w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className='flex flex-col justify-center items-center mt-5'>
+          <div className='h-5 w-5 rounded-full bg-[#915eff]' />
+        </div>
+      </div>
+    </section>
   )
 }
 
