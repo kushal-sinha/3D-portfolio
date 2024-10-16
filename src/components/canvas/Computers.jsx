998f2b01213513dfc889560080ded3c26cff153e
kushal-sinha <kushal147258369@gmail.com>
2024-10-16 20:54:54 +0530

    made changes

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -4,8 +4,21 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
-const Computers = ({ isMobile }) => {
+const Computers = ({ screenSize }) => {
   const computer = useGLTF("./desktop_pc/scene.gltf");
+
+  const getScaleAndPosition = () => {
+    switch (screenSize) {
+      case 'small':
+        return { scale: 0.5, position: [0, -2.5, -1.5] };
+      case 'medium':
+        return { scale: 0.6, position: [0, -2.8, -1.8] };
+      default:
+        return { scale: 0.75, position: [0, -3.25, -1.5] };
+    }
+  };
+
+  const { scale, position } = getScaleAndPosition();
 
   return (
     <mesh>
@@ -21,8 +34,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={scale}
+        position={position}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -30,40 +43,49 @@ const Computers = ({ isMobile }) => {
 };
 
 const ComputersCanvas = () => {
-  const [isMobile, setIsMobile] = useState(false);
+  const [screenSize, setScreenSize] = useState('large');
 
   useEffect(() => {
-    const mediaQuery = window.matchMedia("(max-width: 500px)");
-    setIsMobile(mediaQuery.matches);
-
-    const handleMediaQueryChange = (event) => {
-      setIsMobile(event.matches);
+    const handleResize = () => {
+      if (window.innerWidth < 500) setScreenSize('small');
+      else if (window.innerWidth < 768) setScreenSize('medium');
+      else setScreenSize('large');
     };
 
-    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initial call
 
-    return () => {
-      mediaQuery.removeEventListener("change", handleMediaQueryChange);
-    };
+    return () => window.removeEventListener('resize', handleResize);
   }, []);
+
+  const getCameraProps = () => {
+    switch (screenSize) {
+      case 'small':
+        return { position: [30, 2, 5], fov: 30 };
+      case 'medium':
+        return { position: [18, 2.5, 5], fov: 28 };
+      default:
+        return { position: [20, 3, 5], fov: 25 };
+    }
+  };
 
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <Canvas
         frameloop='demand'
         shadows
-        camera={{ position: [20, 3, 5], fov: 25 }}
+        camera={getCameraProps()}
         gl={{ preserveDrawingBuffer: true }}
       >
         <Suspense fallback={<CanvasLoader />}>
-          {!isMobile && ( // Disable OrbitControls on mobile
-            <OrbitControls
-              enableZoom={false}
-              maxPolarAngle={Math.PI / 2}
-              minPolarAngle={Math.PI / 2}
-            />
-          )}
-          <Computers isMobile={isMobile} />
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+            enablePan={false}
+            enableRotate={screenSize !== 'large'}
+          />
+          <Computers screenSize={screenSize} />
         </Suspense>
         <Preload all />
       </Canvas>
