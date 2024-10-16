54d0e81a93f77062b769fe0886c74d0d02278beb
kushal-sinha <kushal147258369@gmail.com>
2024-10-16 23:04:12 +0530

    refactored

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -83,7 +83,7 @@ const ComputersCanvas = () => {
             maxPolarAngle={Math.PI / 2}
             minPolarAngle={Math.PI / 2}
             enablePan={false}
-            enableRotate={screenSize !== 'large'}
+            enableRotate={screenSize === 'large'}
           />
           <Computers screenSize={screenSize} />
         </Suspense>
